9d7b1ac2a6b0f6bd56ddbe7da6e0be10ff078b33
Rens Breur <rens.breur@live.com>
2022-08-07 16:56:29 +0200

    Retrieve terminal window size from system

--- Sources/SwiftTUI/RunLoop/Application.swift
+++ Sources/SwiftTUI/RunLoop/Application.swift
@@ -19,24 +19,27 @@ public class Application<I: View> {
         control = node.control!
 
         window = Window()
-        window.layer.frame.size = Size(width: 100, height: 50)
         window.addControl(control)
 
         window.firstResponder = control.firstSelectableElement
         window.firstResponder?.becomeFirstResponder()
 
-        control.layout(size: Size(width: 100, height: 50))
         renderer = Renderer(layer: window.layer)
     }
 
     public func start() {
         setInputMode()
-
+        updateWindowSize()
+        control.layout(size: window.layer.frame.size)
         renderer.draw()
 
         let stdInSource = DispatchSource.makeReadSource(fileDescriptor: STDIN_FILENO, queue: .main)
         stdInSource.setEventHandler(qos: .default, flags: [], handler: self.handleInput)
         stdInSource.resume()
+
+        let sigWinChSource = DispatchSource.makeSignalSource(signal: SIGWINCH, queue: .main)
+        sigWinChSource.setEventHandler(qos: .default, flags: [], handler: self.handleWindowSizeChange)
+        sigWinChSource.resume()
 
         dispatchMain()
 
@@ -57,7 +60,6 @@ public class Application<I: View> {
         var char: UInt8 = 0
         _ = read(STDIN_FILENO, &char, 1)
 
-        // Handle key
         if arrowKeyParser.parse(character: char) {
             guard let key = arrowKeyParser.arrowKey else { return }
             arrowKeyParser.arrowKey = nil
@@ -81,14 +83,31 @@ public class Application<I: View> {
             window.firstResponder?.handleEvent(char)
         }
 
-        // Update
+        runLoopCycleEnd()
+    }
+
+    private func runLoopCycleEnd() {
         for node in Node.invalidatedNodes {
             node.update(using: node.viewWrapper)
         }
         Node.invalidatedNodes = []
 
-        node.control!.layout(size: Size(width: 100, height: 50))
+        control.layout(size: window.layer.frame.size)
         renderer.update()
+    }
+
+    private func handleWindowSizeChange() {
+        updateWindowSize()
+        runLoopCycleEnd()
+    }
+
+    private func updateWindowSize() {
+        var size = winsize()
+        guard ioctl(STDOUT_FILENO, UInt(TIOCGWINSZ), &size) == 0 else {
+            assertionFailure("Could not get window size")
+            return
+        }
+        window.layer.frame.size = Size(width: Int(size.ws_col), height: Int(size.ws_row))
     }
 
 }
