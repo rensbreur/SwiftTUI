0149e654170977d35390885fcd92bb509ba5f39d
Rens Breur <rens.breur@live.com>
2022-08-08 19:35:04 +0200

    Update screenshot

--- Examples/Numbers/Sources/Numbers/ContentView.swift
+++ Examples/Numbers/Sources/Numbers/ContentView.swift
@@ -5,18 +5,14 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
-            Button("More") { counter += 1 }
-            Button("Less") { counter -= 1 }
-            HStack {
-                if counter > 0 {
-                    ForEach(1 ... counter, id: \.self) { i in
-                        Text("Number \(i)")
-                    }
-                    .border()
-                } else {
-                    Text("Create more items")
-                }
+            Button("Add number") { counter += 1 }
+            if counter > 1 {
+                Button("Remove number") { counter -= 1 }
             }
+            ForEach(1 ... counter, id: \.self) { i in
+                Text("Number \(i)")
+            }
+            .border()
         }
         .padding()
     }
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 SwiftTUI brings SwiftUI to the terminal. It provides an API similar to SwiftUI to build terminal applications with a text-based user interface.
 
-![](screen_rec.gif)
+![](screenshot.png)
 
 ### What is working
 
