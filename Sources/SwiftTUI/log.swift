7707e695a4895a966e1a0fac4fcc374522713082
Rens Breur <rens.breur@live.com>
2022-08-08 17:50:18 +0200

    Always log to the same log file

--- Sources/SwiftTUI/log.swift
+++ Sources/SwiftTUI/log.swift
@@ -1,7 +1,11 @@
 import Foundation
 
-/// To enable logging, create an empty file or a named socket
-/// as `swift_tui_log` in the tmp directory.
+/// To enable logging, create an empty file under `/tmp/swift_tui_log`.
+///
+/// You can then also monitor the log live:
+/// ```
+/// tail -f /tmp/swift_tui_log
+/// ```
 func log(_ item: Any) {
     print(item, to: &logStream)
 }
@@ -10,7 +14,7 @@ var logStream = LogStream()
 
 struct LogStream: TextOutputStream {
     func write(_ string: String) {
-        let logURL = FileManager.default.temporaryDirectory.appendingPathComponent("swift_tui_log")
+        let logURL = URL(fileURLWithPath: "/tmp/swift_tui_log")
         guard let data = string.data(using: .utf8) else {
             assertionFailure("Cannot write to log")
             return
