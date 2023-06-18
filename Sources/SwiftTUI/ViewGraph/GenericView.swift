478efdcaa5f013df972266ab33c981f41b0209fb
Rens Breur <rens.breur@live.com>
2023-06-18 12:08:20 +0200

    Update generic and primitive view documentation

--- Sources/SwiftTUI/ViewGraph/GenericView.swift
+++ Sources/SwiftTUI/ViewGraph/GenericView.swift
@@ -1,5 +1,10 @@
 import Foundation
 
+/// This is the interface to any view as the view graph is concerned.
+///
+/// Such a generic view can be either a `PrimitiveView`, meaning it has custom logic to build
+/// and update the nodes, or a `ComposedView`, meaning it is created using the familiar `View`
+/// struct.
 protocol GenericView {
     func buildNode(_ node: Node)
     func updateNode(_ node: Node)
