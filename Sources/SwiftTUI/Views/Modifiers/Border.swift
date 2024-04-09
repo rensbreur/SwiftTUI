ba181a0067882f7567ada69459c4c76f98fec311
Rens Breur <rens.breur@live.com>
2024-04-09 18:01:30 +0200

    Make border style customizable

--- Sources/SwiftTUI/Views/Modifiers/Border.swift
+++ Sources/SwiftTUI/Views/Modifiers/Border.swift
@@ -1,14 +1,64 @@
 import Foundation
 
 public extension View {
-    func border(_ color: Color? = nil) -> some View {
-        return Border(content: self, color: color)
+    func border(_ color: Color? = nil, style: BorderStyle = .default) -> some View {
+        return Border(content: self, color: color, style: style)
+    }
+}
+
+public struct BorderStyle {
+    public let topLeft: Character
+    public let top: Character
+    public let topRight: Character
+    public let left: Character
+    public let right: Character
+    public let bottomLeft: Character
+    public let bottom: Character
+    public let bottomRight: Character
+
+    public init(topLeft: Character, top: Character, topRight: Character, left: Character, right: Character, bottomLeft: Character, bottom: Character, bottomRight: Character) {
+        self.topLeft = topLeft
+        self.top = top
+        self.topRight = topRight
+        self.left = left
+        self.right = right
+        self.bottomLeft = bottomLeft
+        self.bottom = bottom
+        self.bottomRight = bottomRight
+    }
+
+    public init(topLeft: Character, topRight: Character, bottomLeft: Character, bottomRight: Character, horizontal: Character, vertical: Character) {
+        self.topLeft = topLeft
+        self.top = horizontal
+        self.topRight = topRight
+        self.left = vertical
+        self.right = vertical
+        self.bottomLeft = bottomLeft
+        self.bottom = horizontal
+        self.bottomRight = bottomRight
+    }
+
+    public static var `default`: BorderStyle {
+        BorderStyle(topLeft: "┌", topRight: "┐", bottomLeft: "└", bottomRight: "┘", horizontal: "─", vertical: "│")
+    }
+
+    public static var rounded: BorderStyle {
+        BorderStyle(topLeft: "╭", topRight: "╮", bottomLeft: "╰", bottomRight: "╯", horizontal: "─", vertical: "│")
+    }
+
+    public static var heavy: BorderStyle {
+        BorderStyle(topLeft: "┏", topRight: "┓", bottomLeft: "┗", bottomRight: "┛", horizontal: "━", vertical: "┃")
+    }
+
+    public static var double: BorderStyle {
+        BorderStyle(topLeft: "╔", topRight: "╗", bottomLeft: "╚", bottomRight: "╝", horizontal: "═", vertical: "║")
     }
 }
 
 private struct Border<Content: View>: View, PrimitiveView, ModifierView {
     let content: Content
     let color: Color?
+    let style: BorderStyle
     @Environment(\.foregroundColor) var foregroundColor: Color
     
     static var size: Int? { Content.size }
@@ -26,16 +76,18 @@ private struct Border<Content: View>: View, PrimitiveView, ModifierView {
     
     func passControl(_ control: Control) -> Control {
         if let borderControl = control.parent { return borderControl }
-        let borderControl = BorderControl(color: color ?? foregroundColor)
+        let borderControl = BorderControl(color: color ?? foregroundColor, style: style)
         borderControl.addSubview(control, at: 0)
         return borderControl
     }
     
     private class BorderControl: Control {
         var color: Color
-        
-        init(color: Color) {
+        var style: BorderStyle
+
+        init(color: Color, style: BorderStyle) {
             self.color = color
+            self.style = style
         }
         
         override func size(proposedSize: Size) -> Size {
@@ -61,22 +113,24 @@ private struct Border<Content: View>: View, PrimitiveView, ModifierView {
             var char: Character?
             if position.line == 0 {
                 if position.column == 0 {
-                    char = "┌"
+                    char = style.topLeft
                 } else if position.column == layer.frame.size.width - 1 {
-                    char = "┐"
+                    char = style.topRight
                 } else {
-                    char = "─"
+                    char = style.top
                 }
             } else if position.line == layer.frame.size.height - 1 {
                 if position.column == 0 {
-                    char = "└"
+                    char = style.bottomLeft
                 } else if position.column == layer.frame.size.width - 1 {
-                    char = "┘"
+                    char = style.bottomRight
                 } else {
-                    char = "─"
+                    char = style.bottom
                 }
-            } else if position.column == 0 || position.column == layer.frame.size.width - 1 {
-                char = "│"
+            } else if position.column == 0 {
+                char = style.left
+            } else if position.column == layer.frame.size.width - 1 {
+                char = style.right
             }
             return char.map { Cell(char: $0, foregroundColor: color) }
         }
