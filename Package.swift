3351db65f1d4720fcd8a3f80157e2ffa7363ec36
Rens Breur <rens.breur@live.com>
2024-04-22 19:19:07 +0200

    Add Swift-DocC Plugin dependency

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ let package = Package(
             targets: ["SwiftTUI"]),
     ],
     dependencies: [
+         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
     ],
     targets: [
         .target(
